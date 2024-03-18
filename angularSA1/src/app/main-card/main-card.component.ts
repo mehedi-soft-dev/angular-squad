c3e98df60b755197013fef8d30c0afec2c238fe7
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2024-03-18 15:24:15 +0600

    add standalone component

--- angularSA1/src/app/main-card/main-card.component.ts
+++ angularSA1/src/app/main-card/main-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from '../user-card/user-card.component';
 
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
-  styleUrl: './main-card.component.css'
+  styleUrl: './main-card.component.css',
 })
 export class MainCardComponent {
 
